12d350f431df4b3e1fe6a4592e90d91652f0b2ed
ys <woosud1763@pusan.ac.kr>
2024-02-01 15:14:06 +0900

    Made SharedMemoryTest

--- original/include/OriSharedMemory.hpp
+++ original/include/OriSharedMemory.hpp
@@ -7,28 +7,10 @@
 
 #include "Eigen/Dense"
 
-#define CMD_dT              0.002
-#define HIGH_CONTROL_dT     0.02
-#define LOW_CONTROL_dT      0.002
-#define VISUAL_dT           0.01
-#define IMU_dT              0.005
-#define ESTIMATOR_dT        0.002
+
 #define MAX_COMMAND_DATA    10
 #define MAX_CUSTOM_DATA     20
-#define PI                  3.141592
-#define R2D                 57.2957802
-#define D2R                 0.0174533
-#define MPC_HORIZON         5
 
-
-typedef struct _UI_COMMAND_
-{
-    int userCommand;
-    int gaitCommand;
-    char userParamChar[MAX_COMMAND_DATA];
-    int userParamInt[MAX_COMMAND_DATA];
-    double userParamDouble[MAX_COMMAND_DATA];
-} UI_COMMAND, * pUI_COMMAND;
 
 typedef struct _SHMORI_
 {
